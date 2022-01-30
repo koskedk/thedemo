7310d5b7445d68d3900f793db49af387baa42ce3
Koske Kimutai <koskedk@live.com>
2022-01-31 00:24:08 +0300

    initial

---
+++ app/src/main/java/org/afyahmis/Demo.java
@@ -0,0 +1,10 @@
+package org.afyahmis;
+
+import org.afyahmis.core.Hello;
+
+public class Demo{
+	public static void main(String[] args){
+		Hello h=new Hello();
+		System.out.println(h.loadGreeting());
+	}
+}
---
+++ lib/src/main/java/org/afyahmis/core/Hello.java
@@ -0,0 +1,7 @@
+package org.afyahmis.core;
+
+public class Hello {
+  public String loadGreeting() {
+    return "Hello Maun !";
+  }
+}
